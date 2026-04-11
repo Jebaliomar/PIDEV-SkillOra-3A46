f52d8c041373995f451fc15b8da204d5fa3d3d30
sarra-souidi <sarrasouidi28@gmail.com>
2026-04-11 11:26:40 +0100

    AjouterEvaluation.fxml

---
+++ src/main/java/tn/esprit/controllers/EvaluationController.java
@@ -0,0 +1,11 @@
+package tn.esprit.controllers;
+
+import javafx.fxml.FXML;
+
+public class EvaluationController {
+
+    @FXML
+    private void ajouterEvaluation() {
+        System.out.println("Bouton Ajouter cliqué");
+    }
+}
